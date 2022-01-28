14fbe01cf979eb805cd80efe800e04416442a7a4
whgdkgo7236 <whgdkgo7236@gmail.com>
2022-01-28 17:02:56 +0900

    Initial commit

---
+++ src/main/java/com/icia/dogsevice/DogSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.icia.dogsevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogSeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogSeviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icia/dogsevice/DogSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.dogsevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogSeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
